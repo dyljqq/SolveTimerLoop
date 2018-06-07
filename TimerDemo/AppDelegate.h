9c6e19fed870d935e7ac196ed55da05cd4630f13
jiqinqiang <jiqinqiang@lukou.com>
2018-06-07 15:07:38 +0800

    Initial Commit

---
+++ TimerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TimerDemo
+//
+//  Created by 季勤强 on 2018/6/7.
+//  Copyright © 2018年 dyljqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TimerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TimerDemo
+//
+//  Created by 季勤强 on 2018/6/7.
+//  Copyright © 2018年 dyljqq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
